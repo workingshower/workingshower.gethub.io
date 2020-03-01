d777f9a30838ab824314a10a7fc2347f9daecc09
Will Harrison <willharrisoncopy@gmail.com>
2020-03-01 00:19:55 -0600

    script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+
+        function ani(){
+          document.getElementById('img').className ='LeftTurn';
+        }
+
+          function ani2(){
+          document.getElementById('img2').className ='RightTurn';
+        }
+
+        function delay (URL) {
+        setTimeout( function() { window.location = URL }, 500 );
+        }
